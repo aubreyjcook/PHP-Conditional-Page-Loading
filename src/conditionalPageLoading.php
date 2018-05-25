533ee540287114dee424b13960be319883c72357
aubreyjcook <aubreyjcook@gmail.com>
2018-05-24 20:52:26 -0500

    load css file method written

--- src/conditionalPageLoading.php
+++ src/conditionalPageLoading.php
@@ -2,4 +2,10 @@
 /* this gets the current working document name and extension as a variable, eg. "index.php" */
 $current_page = basename($_SERVER['PHP_SELF']);
 
+function loadCSSFile($fileName){
+  if (file_exists("css/" . $fileName)){
+    echo '<link href="css/' . $fileName . '" rel="stylesheet">';
+  }
+}
+
 ?>
