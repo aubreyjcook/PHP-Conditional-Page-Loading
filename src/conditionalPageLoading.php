de833f13ef1bf9ed76ce1c083e531cb00a1c6268
Aubrey Cook <aubreyjcook@gmail.com>
2018-06-20 15:46:05 -0500

    loadCSSCDN function created

--- src/conditionalPageLoading.php
+++ src/conditionalPageLoading.php
@@ -4,10 +4,16 @@ $current_page = basename($_SERVER['PHP_SELF']);
 /* this gets the page name without the extension: eg. 'index.php' = 'index' */
 $current_doc_name = preg_replace('/\.[^.]+$/','',$current_page);
 
+function loadCSSCDN($href){
+  echo '<link rel="stylesheet" href="' . $href . '" crossorigin="anonymous">';
+}
+
 function loadCSSFile($fileName){
   if (file_exists("css/" . $fileName . '.css')){
     echo '<link href="css/' . $fileName . '.css " rel="stylesheet">';
   }
 }
+
+// TODO: Create loadJSFile function that takes a js file location or cdn parameter as a string and echos it
 
 ?>
